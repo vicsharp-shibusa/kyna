7c99438f5a1c70cb3bdfd51f93e4fb8a203a6e09
Vic Sharp <vicsharp@shibusa.io>
2024-05-06 20:49:30 -0500

    Added dividends table to financials database SQL.

--- database/postgresql/create_prices_tables.sql
+++ database/postgresql/create_prices_tables.sql
@@ -16,6 +16,25 @@ CREATE TABLE IF NOT EXISTS public.splits
   PRIMARY KEY (source, code, date_split)
 );
 
+CREATE TABLE IF NOT EXISTS public.dividends
+(
+  source TEXT NOT NULL,
+  code TEXT NOT NULL,
+  type TEXT NOT NULL,
+  declaration_date DATE NULL,
+  ex_dividend_date DATE NULL,
+  pay_date DATE NULL,
+  record_date DATE NULL,
+  frequency INT NULL,
+  amount NUMERIC(22,4) NOT NULL,
+  created_ticks_utc BIGINT NOT NULL,
+  updated_ticks_utc BIGINT NOT NULL,
+  created_utc TIMESTAMP WITH TIME ZONE GENERATED ALWAYS AS (to_timestamp((created_ticks_utc - 621355968000000000) / 10000000)) STORED,
+  updated_utc TIMESTAMP WITH TIME ZONE GENERATED ALWAYS AS (to_timestamp((updated_ticks_utc - 621355968000000000) / 10000000)) STORED,
+  process_id UUID NULL,
+  PRIMARY KEY (source, code, type, declaration_date)
+);
+
 CREATE TABLE IF NOT EXISTS public.eod_prices
 (
   source TEXT NOT NULL,
